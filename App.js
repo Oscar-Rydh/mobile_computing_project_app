2402c5b4d9f5a4fe61a7da3fd8fc068becb3d45b
Oscar Rydh <oscar.rydh.93@gmail.com>
2018-04-02 19:18:39 +0900

    Added dynamic button switches based on question, Free input TODO

--- App.js
+++ App.js
@@ -8,9 +8,17 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
     const date = new Date()
-    this.state = { questions: questions, userId: 1234, currentDate: date.getDate(), currentQuestion: "", currentQuestionId: 1 }
+    this.state = {
+      questions: questions,
+      userId: 1234,
+      currentDate: date.getDate(),
+      currentQuestion: "",
+      currentQuestionId: 1,
+      questionsToAnswer: true,
+      scaleButton: false
+    }
     // For testing
-    //this.resetState();
+    this.resetState();
   }
 
   async resetState() {
@@ -50,43 +58,102 @@ export default class App extends React.Component {
       Notifier.stopNotifications();
     Notifier.startAllNotifications();
     const storedState = await this.getState()
-    if (storedState !== null && storedState.currentDate === this.state.currentDate) {
+    if (storedState !== null && storedState.currentDate === this.state.currentDate) {
       this.setState(storedState);
     }
     this._setCurrentQuestion();
   }
 
-  async _updateQuestionList() {
+  async _updateQuestionList(buttonValue) {
     delete this.state.questions[this.state.currentQuestionId];
     const newId = parseInt(this.state.currentQuestionId) + 1;
-    await this.setState({questions: this.state.questions, currentQuestionId: newId })
+    await this.setState({ questions: this.state.questions, currentQuestionId: newId })
     this.saveState();
     this._setCurrentQuestion();
   }
 
   // Hur ska vi göra detta? 
   _setCurrentQuestion() {
-    this.setState({currentQuestion: this.state.questions[this.state.currentQuestionId]})
+    const date = new Date()
+    const currentHour = date.getHours();
+    const currentId = this.state.currentQuestionId;
+    if ((currentHour > 5 && currentId < 19) ||
+      (currentHour > 11 && currentId < 35) ||
+      (currentHour > 16 && currentId < 51) ||
+      currentHour > 21
+    ) {
+      this.setState({ currentQuestion: this.state.questions[this.state.currentQuestionId], questionsToAnswer: true })
+      if (currentId === 18 || currentId === 19 || currentId === 34 || currentId === 35 || currentId === 50 ||
+        currentId === 51 || currentId === 58 || currentId === 59) {
+          this.setState({isScale: true})
+        } else {
+          this.setState({isScale: false})
+        }
+    } else {
+      this.setState({ currentQuestion: "Please wait before answering more questions", questionsToAnswer: false, isScale: false })
+    }
+  }
+
+  _renderYesNoButtons() {
+    if (this.state.questionsToAnswer && !this.state.isScale) {
+      return (
+        <View style={styles.buttonContainer}>
+          <View style={styles.button} >
+            <Button
+              onPress={() => this._updateQuestionList(0)}
+              title="yes"
+            />
+          </View>
+          <View style={styles.button} >
+            <Button style={styles.button}
+              onPress={() => this._updateQuestionList(1)}
+              title="no"
+            />
+          </View>
+        </View>
+      )
+    }
+  }
+
+  _renderScaleButtons() {
+    if (this.state.questionsToAnswer && this.state.isScale) {
+      return (
+        <View style={styles.buttonContainer}>
+          <View style={styles.button} >
+            <Button
+              onPress={() => this._updateQuestionList(1)}
+              title="1"
+            />
+          </View>
+          <View style={styles.button} >
+            <Button style={styles.button}
+              onPress={() => this._updateQuestionList(2)}
+              title="2"
+            />
+          </View>
+          <View style={styles.button} >
+            <Button style={styles.button}
+              onPress={() => this._updateQuestionList(3)}
+              title="3"
+            />
+          </View>
+          <View style={styles.button} >
+            <Button style={styles.button}
+              onPress={() => this._updateQuestionList(4)}
+              title="4"
+            />
+          </View>
+        </View>
+      )
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Question: {this.state.currentQuestion}</Text>
-        <View style={styles.buttonContainer} >
-          <View style={styles.button} >
-            <Button
-              onPress={() => this._updateQuestionList()}
-              title="yes"
-            />
-          </View>
-          <View style={styles.button} >
-            <Button style={styles.button}
-              onPress={() => this._updateQuestionList()}
-              title="no"
-            />
-          </View>
-        </View>
+        {this._renderYesNoButtons()}
+        {this._renderScaleButtons()}
       </View>
     );
   }
@@ -103,6 +170,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   button: {
-    padding: 80
+    padding: 20
   }
 });
