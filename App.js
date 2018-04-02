68611396708ad43889d993b21c22853b426acb7f
Oscar Rydh <oscar.rydh.93@gmail.com>
2018-04-02 18:07:12 +0900

    Added persistent state store

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
 import Notifier from './util/notifier';
 import questions from './util/questions';
 import QuestionID from './util/QuestionID'
@@ -8,52 +8,81 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
     const date = new Date()
-    this.state = {questions: {}, userId: 1234, currentDate: date.getDate(), currentQuestionId: 1}
+    this.state = { questions: questions, userId: 1234, currentDate: date.getDate(), currentQuestion: "", currentQuestionId: 1 }
+    // For testing
+    //this.resetState();
   }
-  
 
+  async resetState() {
+    try {
+      await AsyncStorage.removeItem('@StateStore:state');
+    } catch (error) {
+      console.log(error)
+      // Error saving data
+    }
+  }
+
+  async saveState() {
+    try {
+      await AsyncStorage.setItem('@StateStore:state', JSON.stringify(this.state));
+    } catch (error) {
+      console.log(error)
+      // Error saving data
+    }
+  }
+
+  async getState() {
+    try {
+      const result = await AsyncStorage.getItem('@StateStore:state');
+      if (result === null) {
+        return null
+      }
+      return JSON.parse(result);
+    } catch (error) {
+      console.log("Got error", error)
+      return null;
+      // Error retrieving data
+    }
+  }
 
   async componentDidMount() {
     let result = await
       Notifier.stopNotifications();
     Notifier.startAllNotifications();
-    this._setCurrentQuestions();
-
+    const storedState = await this.getState()
+    if (storedState !== null && storedState.currentDate === this.state.currentDate) {
+      this.setState(storedState);
+    }
+    this._setCurrentQuestion();
   }
 
-  _updateCurrentQuestion() {
-    this.setState({currentQuestionId: parseInt(this.state.currentQuestionId) + 1})
+  async _updateQuestionList() {
+    delete this.state.questions[this.state.currentQuestionId];
+    const newId = parseInt(this.state.currentQuestionId) + 1;
+    await this.setState({questions: this.state.questions, currentQuestionId: newId })
+    this.saveState();
+    this._setCurrentQuestion();
   }
 
   // Hur ska vi göra detta? 
-  _setCurrentQuestions() {
-    const date = new Date();
-    const currentHour = date.getHours();
-    if (currentHour > 5 && currentHour <= 10) {
-      this.setState({questions: questions.morning, currentQuestionId: Object.keys(questions.morning)[0]});
-    } else if (currentHour > 10 && currentHour <= 15) {
-      this.setState({questions: questions.lunch, currentQuestionId: Object.keys(questions.lunch)[0]});
-    } else if (currentHour > 15 && currentHour <= 21) {
-      this.setState({questions: questions.dinner, currentQuestionId: Object.keys(questions.dinner)[0]});
-    } else {
-      this.setState({questions: questions.all_day, currentQuestionId: Object.keys(questions.all_day)[0]})
-    }
+  _setCurrentQuestion() {
+    this.setState({currentQuestion: this.state.questions[this.state.currentQuestionId]})
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Question: {this.state.questions[this.state.currentQuestionId]}</Text>
+        <Text>Question: {this.state.currentQuestion}</Text>
         <View style={styles.buttonContainer} >
           <View style={styles.button} >
             <Button
-              onPress={() => this._updateCurrentQuestion()}
+              onPress={() => this._updateQuestionList()}
               title="yes"
             />
           </View>
           <View style={styles.button} >
             <Button style={styles.button}
-              onPress={() => _updateCurrentQuestion()}
+              onPress={() => this._updateQuestionList()}
               title="no"
             />
           </View>
