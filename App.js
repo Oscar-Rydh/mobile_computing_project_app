f08ad73ebd325bc1c1a3337afaf7f5b5f8e51b76
Oscar Rydh <oscar.rydh.93@gmail.com>
2018-04-02 16:44:31 +0900

    Made a simple question interface that shows questions based on time

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Notifier from './util/notifier'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Notifier from './util/notifier';
+import questions from './util/questions';
+import QuestionID from './util/QuestionID'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    const date = new Date()
+    this.state = {questions: {}, userId: 1234, currentDate: date.getDate(), currentQuestionId: 1}
+  }
+  
 
 
   async componentDidMount() {
-    let result = await   
+    let result = await
+      Notifier.stopNotifications();
     Notifier.startAllNotifications();
+    this._setCurrentQuestions();
+
+  }
+
+  _updateCurrentQuestion() {
+    this.setState({currentQuestionId: parseInt(this.state.currentQuestionId) + 1})
+  }
+
+  // Hur ska vi göra detta? 
+  _setCurrentQuestions() {
+    const date = new Date();
+    const currentHour = date.getHours();
+    if (currentHour > 5 && currentHour <= 10) {
+      this.setState({questions: questions.morning, currentQuestionId: Object.keys(questions.morning)[0]});
+    } else if (currentHour > 10 && currentHour <= 15) {
+      this.setState({questions: questions.lunch, currentQuestionId: Object.keys(questions.lunch)[0]});
+    } else if (currentHour > 15 && currentHour <= 21) {
+      this.setState({questions: questions.dinner, currentQuestionId: Object.keys(questions.dinner)[0]});
+    } else {
+      this.setState({questions: questions.all_day, currentQuestionId: Object.keys(questions.all_day)[0]})
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Question: {this.state.questions[this.state.currentQuestionId]}</Text>
+        <View style={styles.buttonContainer} >
+          <View style={styles.button} >
+            <Button
+              onPress={() => this._updateCurrentQuestion()}
+              title="yes"
+            />
+          </View>
+          <View style={styles.button} >
+            <Button style={styles.button}
+              onPress={() => _updateCurrentQuestion()}
+              title="no"
+            />
+          </View>
+        </View>
       </View>
     );
   }
@@ -26,4 +70,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    flexDirection: 'row'
+  },
+  button: {
+    padding: 80
+  }
 });
---
+++ util/QuestionID.js
@@ -0,0 +1,84 @@
+/**
+ * Module that helps translate human readable strings to IDs
+ */
+
+module.exports = {
+  morning: {
+    // Default questions
+    had_break: 1,
+    had_focus: 2,
+    was_interruppted: 3,
+    did_procastinate: 4,
+    did_lie: 5,
+    has_messy_workspace: 6,
+    has_eaten: 7,
+    is_super_full: 8,
+    had_water: 9,
+    did_smoke: 10,
+    did_workout: 11,
+    eaten_sweets: 12,
+    well_being: 13,
+    stressed: 14,
+    comment: 15,
+
+    // Special for the morning
+    email_before_workout: 16,
+    has_clear_daygoals: 17,
+    had_alcohol_yesterday: 18,
+    was_drunk_yesterday: 19,
+    had_enough_sleep: 20
+  },
+  lunch: {
+    // Default questions
+    had_break: 21,
+    had_focus: 22,
+    was_interruppted: 23,
+    did_procastinate: 24,
+    did_lie: 25,
+    has_messy_workspace: 26,
+    has_eaten: 27,
+    is_super_full: 28,
+    had_water: 29,
+    did_smoke: 30,
+    did_workout: 31,
+    eaten_sweets: 32,
+    well_being: 33,
+    stressed: 34,
+    comment: 35,
+
+    // Special for lunch
+    too_much_coffee: 36,
+  },
+  dinner: {
+    // Default questions
+    had_break: 37,
+    had_focus: 38,
+    was_interruppted: 39,
+    did_procastinate: 40,
+    did_lie: 41,
+    has_messy_workspace: 42,
+    has_eaten: 43,
+    is_super_full: 44,
+    had_water: 45,
+    did_smoke: 46,
+    did_workout: 47,
+    eaten_sweets: 48,
+    well_being: 49,
+    stressed: 50,
+    comment: 51,
+
+    // Special for dinner
+    too_much_coffee: 52
+  },
+  all_day: {
+    productive_before_lunch: 53,
+
+    too_much_tv: 54,
+    too_much_social_media: 55,
+    regret_did_accept_task: 56,
+    had_enough_water: 57,
+    well_being: 58,
+    stressed: 59,
+    comment: 60,
+  }
+};
